7153f0cd0144c9e131767ddd48e56e371390e9f3
msleigh <msleigh@users.noreply.github.com>
2020-10-06 15:14:40 +0100

    Fixes missing import statement from download script

--- download.py
+++ download.py
@@ -6,6 +6,7 @@
 # URL to download 'Cases by specimen date' for 'UK total' in CSV:
 # https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=csv
 
+import os
 from uk_covid19 import Cov19API
 
 # Construct a filter
